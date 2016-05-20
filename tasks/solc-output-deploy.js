5b8978e0054dc7ff1790bb1df0ea0112b6e3ce94
Aakil Fernandes <aakilfernandes@gmail.com>
2016-05-20 14:47:49 -0400

    make contracts available for eval

--- tasks/solc-output-deploy.js
+++ tasks/solc-output-deploy.js
@@ -1,148 +1,159 @@
-module.exports = function(grunt){
+module.exports = function exportGrunt(grunt) {
 
-	grunt.registerMultiTask('solc-output-deploy',function(){
+  grunt.registerMultiTask('solc-output-deploy', function regiterSolcOutputDeploy() {
 
-		var Web3 = require('web3')
-			,Q = require('q')
-		
-		var done = this.async()
-			,options = this.options({
-				rpchost: 'localhost'
-				,rpcport: 8545
-				,forceRedeploy:false
-				,onDeploy: {}
-			})
-			,web3 = new Web3()
-			,contractsObj = grunt.file.readJSON(options.contracts).contracts
-			,chain = grunt.file.exists(options.chain) ? grunt.file.readJSON(options.chain) : {}
-			,rpcUrl = 'http://'+options.rpchost+':'+options.rpcport
-			,requiredOptionKeys = ['contracts','chain','deploy']
+    const Web3 = require('web3')
+    const Q = require('q')
+    const done = this.async()
+    const options = this.options({
+      rpchost: 'localhost',
+      rpcport: 8545,
+      forceRedeploy: false,
+      onDeploy: {}
+    })
+    const web3 = new Web3()
+    const contractsObj = grunt.file.readJSON(options.contracts).contracts
+    const contracts = {}
+    const chain = grunt.file.exists(options.chain) ? grunt.file.readJSON(options.chain) : {}
+    const rpcUrl = `http://${options.rpchost}:${options.rpcport}`
+    const requiredOptionKeys = ['contracts', 'chain', 'deploy']
 
-		requiredOptionKeys.forEach(function(key){
-			if(options[key]) return;
+    Object.keys(contractsObj).forEach((contractName) => {
+      contractsObj[contractName].abi = JSON.parse(contractsObj[contractName].interface)
+    })
 
-			grunt.log.error('options.'+key,'missing');
-			done(false)
-		})
+    requiredOptionKeys.forEach((key) => {
+      if (options[key]) {
+        return
+      }
+      grunt.log.error(`options.${key}missing`)
+      done(false)
+    })
 
-		web3.setProvider(new web3.providers.HttpProvider(rpcUrl));
+    web3.setProvider(new web3.providers.HttpProvider(rpcUrl))
 
-		var address = options.address || web3.eth.accounts[0]
+    const address = options.address || web3.eth.accounts[0]
 
-		if(!address){
-			grunt.log.error('Could not determine address')
-			done(false)
-		}
+    if (!address) {
+      grunt.log.error('Could not determine address')
+      done(false)
+    }
 
-		grunt.log.writeln('Setting',address,'as default account')
-		web3.eth.defaultAccount = address
+    grunt.log.writeln(`Setting ${address} as default account`)
+    web3.eth.defaultAccount = address
 
-		var contractsToDeploy = options.deploy.filter(function(contractName){
-			if(chain[contractName] && hexify(contractsObj[contractName].runtimeBytecode) == web3.eth.getCode(chain[contractName].address)){
-				grunt.log.writeln(contractName,'already deployed to',chain[contractName].address)
-				return false
-			}else{
-				return true
-			}
-		})
+    const contractsToDeploy = options.deploy.filter((contractName) => {
+      if (chain[contractName] && hexify(contractsObj[contractName].runtimeBytecode) === web3.eth.getCode(chain[contractName].address)) {
+        grunt.log.writeln(`${contractName} already deployed to ${chain[contractName].address}`)
+        contracts[contractName] = web3.eth.contract(contractsObj[contractName].abi).at(contractsObj[contractName].address)
+        return false
+      }
 
-		if(contractsToDeploy.length===0){
-			grunt.log.success('No contracts to deploy')
-			return done(true)
-		}else{
-			grunt.log.writeln(contractsToDeploy.length,'contracts need deployment')
-		}
+      return true
+    })
 
-		if(!web3.isConnected()){
-			grunt.log.error('Web3 cannot connect on',options.rpchost,options.rpcport)
-			return done(false)
-		}		
+    if (contractsToDeploy.length === 0) {
+      grunt.log.success('No contracts to deploy')
+      return done(true)
+    }
 
-		var deployPromises = contractsToDeploy.map(function(contractName){
-			return new Deployment(contractName)
-		})
+    grunt.log.writeln(`${contractsToDeploy.length} contracts need deployment`)
 
-		Q.all(deployPromises).then(function(){
-			grunt.file.write(options.chain,JSON.stringify(chain))
-			grunt.log.success('Deployed all contracts and wrote chain to',options.chain)
-			done(true)
-		})
+    if (!web3.isConnected()) {
+      grunt.log.error(`Web3 cannot connect on ${options.rpchost}:${options.rpcport}`)
+      return done(false)
+    }
 
-		function Deployment(contractName){
-			
-			var deferred = Q.defer()
-				,txParams = {
-					data: hexify(contractsObj[contractName].bytecode)
-					,gasPrice: web3.toHex(web3.eth.gasPrice)
-				}
-
-			txParams.gas = web3.toHex(web3.eth.estimateGas(txParams))
-	
-			web3.eth.sendTransaction(txParams,function(err,txHex){
-				if(err){
-					grunt.log.error('Deploying',contractName,'failed:',err.message)
-					return done(false)
-				}
-
-				grunt.log.writeln('Deploying',contractName,'with transaction',txHex)
-
-				waitForTx(txHex).then(function(transactionReceipt){
-
-					grunt.log.writeln('Deployed',contractName,'to',transactionReceipt.contractAddress)
-
-					if(options.onDeploy[contractName]){
-						var abi = JSON.parse(contractsObj[contractName].interface)
-							,thisContract = web3.eth.contract(abi).at(transactionReceipt.contractAddress)
-
-						options.onDeploy[contractName].forEach(function(snippet){
-							grunt.log.writeln('eval:',snippet)
-							try{
-								eval(snippet);
-							}catch(e){
-								grunt.log.error(e)
-								grunt.file.write(options.chain,JSON.stringify(chain))
-								done(false)
-							}
-							
-						})
-					}
-
-					chain[contractName] = {
-						address:hexify(transactionReceipt.contractAddress)
-						,runtimeBytecode:hexify(web3.eth.getCode(transactionReceipt.contractAddress))
-					}
-
-					deferred.resolve()
-
-				})
-			})
-
-			return deferred.promise
-		}
-
-		function hexify(string){
-			if(string.indexOf('0x')===0)
-				return string
-			else
-				return '0x'+string
-		}
-
-		function waitForTx(txHex){
-			var deferred = Q.defer()
-				,waitInterval = setInterval(function(){
-
-					var txReceipt = web3.eth.getTransactionReceipt(txHex)
-					
-					if(!txReceipt) return;
-
-					clearInterval(waitInterval)
-					deferred.resolve(txReceipt)
-
-				},1000)
-
-			return deferred.promise
-		}
+    deployNextContract()
 
 
-	})
+    function deployNextContract() {
+      if (contractsToDeploy.length > 0) {
+        const deployment = new Deployment(contractsToDeploy[0])
+        deployment.then(() => {
+          contractsToDeploy.shift()
+          deployNextContract()
+        })
+      } else {
+        grunt.file.write(options.chain, JSON.stringify(chain))
+        grunt.log.success(`Deployed all contracts and wrote chain to ${options.chain}`)
+        done(true)
+      }
+    }
+
+    function Deployment(contractName) {
+
+      const deferred = Q.defer()
+      const txParams = {
+        data: hexify(contractsObj[contractName].bytecode),
+        gasPrice: web3.toHex(web3.eth.gasPrice)
+      }
+
+      txParams.gas = web3.toHex(web3.eth.estimateGas(txParams))
+
+      web3.eth.sendTransaction(txParams, (err, txHex) => {
+        if (err) {
+          grunt.log.error(`Deploying ${contractName} failed: ${err.message}`)
+          return done(false)
+        }
+
+        grunt.log.writeln(`Deploying ${contractName} with transaction ${txHex}`)
+
+        waitForTx(txHex).then((transactionReceipt) => {
+
+          grunt.log.writeln(`Deployed ${contractName} to ${transactionReceipt.contractAddress}`)
+
+          chain[contractName] = {
+            address: hexify(transactionReceipt.contractAddress),
+            runtimeBytecode: hexify(web3.eth.getCode(transactionReceipt.contractAddress))
+          }
+
+          if (options.onDeploy[contractName]) {
+            contracts[contractName] = web3.eth.contract(contractsObj[contractName].abi).at(transactionReceipt.contractAddress)
+
+            options.onDeploy[contractName].forEach((snippet) => {
+              grunt.log.writeln(`eval: ${snippet}`)
+              try {
+                eval(snippet)
+              } catch (e) {
+                grunt.log.error(e)
+                grunt.file.write(options.chain, JSON.stringify(chain))
+                done(false)
+              }
+            })
+          }
+
+          deferred.resolve()
+
+        })
+      })
+
+      return deferred.promise
+    }
+
+    function hexify(string) {
+      if (string.indexOf('0x') === 0) {
+        return string
+      }
+      return `0x${string}`
+    }
+
+    function waitForTx(txHex) {
+      const deferred = Q.defer()
+      const waitInterval = setInterval(() => {
+
+        const txReceipt = web3.eth.getTransactionReceipt(txHex)
+
+        if (!txReceipt) {
+          return
+        }
+
+        clearInterval(waitInterval)
+        deferred.resolve(txReceipt)
+
+      }, 1000)
+
+      return deferred.promise
+    }
+  })
 }
